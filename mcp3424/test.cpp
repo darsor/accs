d9fc92a38fe58fa39f3269fe278cec7457c95e3e
Dana Sorensen <dana.r.sorensen@gmail.com>
2016-07-26 21:19:42 +0000

    fixed some tests, everything works first try!

--- mcp3424/test.cpp
+++ mcp3424/test.cpp
@@ -13,7 +13,7 @@ int main() {
     getchar();
 
     config = dev.getConfig();
-    printf("config is %02x, expecting %02x\n", config & ~RDY_MASK, CHANNEL1 | CONTINUOUS | RES_12_BITS | PGAx1);
+    printf("config is 0x%02x, expecting 0x%02x\n", config & ~RDY_MASK, CHANNEL1 | CONTINUOUS | RES_12_BITS | PGAx1);
 
     printf("\nTEST 2: Testing getConfig(). Press enter to continue... ");
     getchar();
@@ -21,7 +21,7 @@ int main() {
     dev.setConfig(CHANNEL4 | ONESHOT | RES_16_BITS | PGAx2);
     usleep(200000);
     config = dev.getConfig();
-    printf("config is %02x, expecting %02x\n", config & ~RDY_MASK, CHANNEL4 | ONESHOT | RES_16_BITS | PGAx2);
+    printf("config is 0x%02x, expecting 0x%02x\n", config & ~RDY_MASK, CHANNEL4 | ONESHOT | RES_16_BITS | PGAx2);
 
     printf("\nTEST 3: Testing getConfig(). Press enter to continue... ");
     getchar();
@@ -29,30 +29,31 @@ int main() {
     dev.setConfig(CHANNEL1 | ONESHOT | RES_18_BITS | PGAx1);
     usleep(400000);
     config = dev.getConfig();
-    printf("config is %02x, expecting %02x\n", config & ~RDY_MASK, CHANNEL1 | ONESHOT | RES_18_BITS | PGAx1);
+    printf("config is 0x%02x, expecting 0x%02x\n", config & ~RDY_MASK, CHANNEL1 | ONESHOT | RES_18_BITS | PGAx1);
 
     printf("\nTEST 4: Testing isReady(). Press enter to continue... ");
     getchar();
     usleep(400000);
-    printf("conversion is %s ready, expected to be ready\n", dev.isReady() ? "" : "not");
+    printf("conversion is %sready, expected to be ready\n", dev.isReady() ? "" : "not ");
 
     printf("\nTEST 5: Testing isReady(). Press enter to continue... ");
     getchar();
-    usleep(400000);
-    printf("conversion is %s ready, expected to be not ready\n", dev.isReady() ? "" : "not");
+    dev.startConversion();
+    usleep(50000);
+    printf("conversion is %sready, expected to be not ready\n", dev.isReady() ? "" : "not ");
 
     printf("\nTEST 6: Testing isReady() and startConversion(). Press enter to continue... ");
     getchar();
-    dev.startConversion();
     usleep(500000);
-    printf("conversion is %s ready, expected to be ready\n", dev.isReady() ? "" : "not");
+    printf("conversion is %sready, expected to be ready\n", dev.isReady() ? "" : "not ");
 
     printf("\nTEST 7: Testing isReady() and startConversion(). Press enter to continue... ");
     getchar();
-    usleep(400000);
-    printf("conversion is %s ready, expected to be not ready\n", dev.isReady() ? "" : "not");
+    dev.startConversion();
+    usleep(500000);
+    printf("conversion is %sready, expected to be ready\n", dev.isReady() ? "" : "not ");
 
-    printf("\nTEST 8: Reading 12-bit values from channel 1. Press enter to continue... ");
+    printf("\nTEST 8: Reading 12-bit values from channel 1 (PGAx1). Press enter to continue... ");
     getchar();
     printf("\n");
 
@@ -60,11 +61,11 @@ int main() {
     usleep(100000);
     for (int i=0; i<10; ++i) {
         data = dev.getConversion();
-        printf("Data point %d: %03x\n", i+1, data);
+        printf("Data point %d: 0x%03x\n", i, data);
         usleep(100000);
     }
 
-    printf("\nTEST 9: Reading 14_bit values from channel 1. Press enter to continue... ");
+    printf("\nTEST 9: Reading 14_bit values from channel 1 (PGAx1). Press enter to continue... ");
     getchar();
     printf("\n");
 
@@ -72,11 +73,11 @@ int main() {
     usleep(100000);
     for (int i=0; i<10; ++i) {
         data = dev.getConversion();
-        printf("Data point %d: %04x\n", i+1, data);
+        printf("Data point %d: 0x%04x\n", i, data);
         usleep(100000);
     }
 
-    printf("\nTEST 10: Reading 16_bit values from channel 1. Press enter to continue... ");
+    printf("\nTEST 10: Reading 16_bit values from channel 1 (PGAx1). Press enter to continue... ");
     getchar();
     printf("\n");
 
@@ -84,11 +85,11 @@ int main() {
     usleep(200000);
     for (int i=0; i<10; ++i) {
         data = dev.getConversion();
-        printf("Data point %d: %04x\n", i+1, data);
+        printf("Data point %d: 0x%04x\n", i, data);
         usleep(200000);
     }
 
-    printf("\nTEST 11: Reading 18_bit values from channel 1. Press enter to continue... ");
+    printf("\nTEST 11: Reading 18_bit values from channel 1 (PGAx1). Press enter to continue... ");
     getchar();
     printf("\n");
 
@@ -96,54 +97,54 @@ int main() {
     usleep(400000);
     for (int i=0; i<10; ++i) {
         data = dev.getConversion();
-        printf("Data point %d: %05x\n", i+1, data);
+        printf("Data point %d: 0x%05x\n", i, data);
         usleep(400000);
     }
 
-    printf("TEST 12: Reading 12-bit values from channel 2. Press enter to continue... ");
+    printf("\nTEST 12: Reading 12-bit values from channel 2 (PGAx2). Press enter to continue... ");
     getchar();
     printf("\n");
 
-    dev.setConfig(CHANNEL2 | CONTINUOUS | RES_12_BITS | PGAx1);
+    dev.setConfig(CHANNEL2 | CONTINUOUS | RES_12_BITS | PGAx2);
     for (int i=0; i<10; ++i) {
         data = dev.getConversion();
-        printf("Data point %d: %03x\n", i+1, data);
+        printf("Data point %d: 0x%03x\n", i, data);
         usleep(100000);
     }
 
-    printf("\nTEST 13: Reading 14_bit values from channel 2. Press enter to continue... ");
+    printf("\nTEST 13: Reading 14_bit values from channel 2 (PGAx2). Press enter to continue... ");
     getchar();
     printf("\n");
 
-    dev.setConfig(CHANNEL2 | CONTINUOUS | RES_14_BITS | PGAx1);
+    dev.setConfig(CHANNEL2 | CONTINUOUS | RES_14_BITS | PGAx2);
     usleep(100000);
     for (int i=0; i<10; ++i) {
         data = dev.getConversion();
-        printf("Data point %d: %04x\n", i+1, data);
+        printf("Data point %d: 0x%04x\n", i, data);
         usleep(100000);
     }
 
-    printf("\nTEST 14: Reading 16_bit values from channel 2. Press enter to continue... ");
+    printf("\nTEST 14: Reading 16_bit values from channel 2 (PGAx2). Press enter to continue... ");
     getchar();
     printf("\n");
 
-    dev.setConfig(CHANNEL2 | CONTINUOUS | RES_16_BITS | PGAx1);
+    dev.setConfig(CHANNEL2 | CONTINUOUS | RES_16_BITS | PGAx2);
     usleep(200000);
     for (int i=0; i<10; ++i) {
         data = dev.getConversion();
-        printf("Data point %d: %04x\n", i+1, data);
+        printf("Data point %d: 0x%04x\n", i, data);
         usleep(200000);
     }
 
-    printf("\nTEST 15: Reading 18_bit values from channel 2. Press enter to continue... ");
+    printf("\nTEST 15: Reading 18_bit values from channel 2 (PGAx2). Press enter to continue... ");
     getchar();
     printf("\n");
 
-    dev.setConfig(CHANNEL2 | CONTINUOUS | RES_18_BITS | PGAx1);
+    dev.setConfig(CHANNEL2 | CONTINUOUS | RES_18_BITS | PGAx2);
     usleep(400000);
     for (int i=0; i<10; ++i) {
         data = dev.getConversion();
-        printf("Data point %d: %05x\n", i+1, data);
+        printf("Data point %d: 0x%05x\n", i, data);
         usleep(400000);
     }
 
