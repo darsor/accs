e6b6c67a779f3e6dd529bb81b74ed07c2e4ce16c
Dana Sorensen <dana.r.sorensen@gmail.com>
2017-05-11 18:17:15 +0000

    updated device channels for new layout

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "mcp3424/mcp3424.h"    // for MCP3424 interface
+#include "mcp4725/mcp4725.h"    // for MCP4725 interface
 #include "ads1148/ads1148.h"    // for ADS1148 interface
 #include "pump/dcmotor.h"       // for pump interface
 #include "cosmos/packets.h"     // for packet definitions
@@ -12,19 +13,12 @@
 #include <unistd.h>             // for getpid() and usleep()
 #include <chrono>               // for timestamps and timing
 #include <sys/sysinfo.h>        // for total memory used
-
-//TODO: set the rpm pin
-#define RPM_PIN     1
+#include <math.h>               // for round()
 
 // this queue provides two concurrent threads: a tlm_queue and cmd_queue
 // any packet pushed to the tlm_queue will be sent to COSMOS
 // any packet received will be found in the cmd_queue
 CosmosQueue queue(4810, 20000, 8);
-
-// function prototype for the ISR (defined at the bottom) that increments
-// count each time the Raspberry Pi detects a rising edge on the RPM line
-void rpmISR();
-volatile unsigned long count = 0;
 
 // function prototype for getTimestamp() (defined at the bottom), which
 // returns the time in microseconds since unix epoch
@@ -50,14 +44,14 @@ void ads1148_thread() {
             }
         }
 
-        //TODO: set ADS1148 settings (VREP, IEXT, etc.)
+        // set ADS1148 settings (VREP, IEXT, etc.)
         try {
-            dev->setMux(AIN2, AIN0);
+            dev->setMux(AIN0, AIN1);
             dev->setVREFstate(VREF_ON);
             dev->setVREFsource(VREF_REF0);
             dev->setPGA(ADS1148_PGAx32);
             dev->setDataRate(DATA_20SPS);
-            dev->setDACpins(DAC_AIN2, DAC_AIN0);
+            dev->setDACpins(DAC_AIN0, DAC_AIN1);
             dev->setDACmagnitude(DAC_500uA);
         } catch (int e) {
             printf("failed to write ADS1148 settings\n");
@@ -73,20 +67,20 @@ void ads1148_thread() {
                 tPacket = new TempPacket;
 
                 // get channel 1 conversion and switch to channel 2
-                dev->getConversion(conv, AIN1, AIN3);
-                dev->setDACpins(DAC_AIN1, DAC_AIN3);
+                dev->getConversion(conv, AIN2, AIN3);
+                dev->setDACpins(DAC_AIN2, DAC_AIN3);
                 tPacket->time1 = conv.timestamp;
                 tPacket->temp1 = conv.code;
 
                 // get channel 2 conversion and switch to channel 3
-                dev->getConversion(conv, AIN5, AIN7);
-                dev->setDACpins(DAC_AIN5, DAC_AIN7);
+                dev->getConversion(conv, AIN4, AIN5);
+                dev->setDACpins(DAC_AIN4, DAC_AIN5);
                 tPacket->time2 = conv.timestamp;
                 tPacket->temp2 = conv.code;
 
                 // get channel 3 conversion and switch to channel 1
-                dev->getConversion(conv, AIN2, AIN0);
-                dev->setDACpins(DAC_AIN2, DAC_AIN0);
+                dev->getConversion(conv, AIN0, AIN1);
+                dev->setDACpins(DAC_AIN0, DAC_AIN1);
                 tPacket->time3 = conv.timestamp;
                 tPacket->temp3 = conv.code;
 
@@ -102,9 +96,9 @@ void ads1148_thread() {
     }
 }
 
-// this thread handles everything to do with the first MCP3424
-// it collects and queues the pressure packets
-void mcp3424_thread1() {
+// this thread handles everything to do with the MCP3424
+// it collects and queues the pressure and power packets
+void mcp3424_thread() {
     MCP3424* dev = nullptr;
     while (true) {
         // try opening the MCP3424 every ten seconds until it succeeds
@@ -119,48 +113,50 @@ void mcp3424_thread1() {
             }
         }
 
-        PressurePacket* pPacket = nullptr;
-        //LevelPacket* lPacket = nullptr;
+        PressurePacket* prsPacket = nullptr;
+        PowerPacket* pwrPacket = nullptr;
         Packet* cmdPacket = nullptr;
         float vZero, sZero, pZero;
         vZero = sZero = pZero = 0.0;
 
         try {
-            // get and queue the PressurePacket repeatedly
+            // get and queue the PressurePacket and PowerPacket repeatedly
             while (true) {
-                pPacket = new PressurePacket;
+                prsPacket = new PressurePacket;
                 dev->setConfig(CHANNEL1 | ONESHOT | RES_16_BITS | PGAx2);
                 dev->startConversion();
                 while (!dev->isReady()) usleep(1000);
-                pPacket->venturiTime = getTimestamp();
-                pPacket->venturiPressure = dev->getConversion();
+                prsPacket->venturiTime = getTimestamp();
+                prsPacket->venturiPressure = dev->getConversion();
 
                 dev->setConfig(CHANNEL2 | ONESHOT | RES_16_BITS | PGAx2);
                 dev->startConversion();
                 while (!dev->isReady()) usleep(1000);
-                pPacket->staticTime = getTimestamp();
-                pPacket->staticPressure = dev->getConversion();
+                prsPacket->staticTime = getTimestamp();
+                prsPacket->staticPressure = dev->getConversion();
 
-                dev->setConfig(CHANNEL3 | ONESHOT | RES_16_BITS | PGAx2);
+                dev->setConfig(CHANNEL4 | ONESHOT | RES_16_BITS | PGAx2);
                 dev->startConversion();
                 while (!dev->isReady()) usleep(1000);
-                pPacket->pumpTime = getTimestamp();
-                pPacket->pumpPressure = dev->getConversion();
+                prsPacket->pumpTime = getTimestamp();
+                prsPacket->pumpPressure = dev->getConversion();
 
-                pPacket->venturiZero = vZero;
-                pPacket->staticZero  = sZero;
-                pPacket->pumpZero    = pZero;
-                queue.push_tlm(pPacket);
+                prsPacket->venturiZero = vZero;
+                prsPacket->staticZero  = sZero;
+                prsPacket->pumpZero    = pZero;
+                queue.push_tlm(prsPacket);
 
-                // liquid level sensor on channel 4
-                /*dev->setConfig(CHANNEL4 | ONESHOT | RES_16_BITS | PGAx8);
+                // pump current sensor on channel 3
+                dev->setConfig(CHANNEL4 | ONESHOT | RES_16_BITS | PGAx8);
                 dev->startConversion();
-                lPacket = new LevelPacket;
+                pwrPacket = new PowerPacket;
                 while (!dev->isReady()) usleep(1000);
-                lPacket->timestamp = getTimestamp();
-                lPacket->value = dev->getConversion();
+                pwrPacket->amperageTime = getTimestamp();
+                pwrPacket->amperage = dev->getConversion();
+                pwrPacket->voltageTime = 0;
+                pwrPacket->voltage = 0;
 
-                queue.push_tlm(lPacket); */
+                queue.push_tlm(pwrPacket);
 
                 if (queue.cmdSize() > 0 && queue.cmd_front_id() == ZERO_PRES_ID) {
                     printf("received zero cmd, new zeroes are:\n");
@@ -178,72 +174,6 @@ void mcp3424_thread1() {
             delete dev;
             continue;
         }
-    }
-}
-
-// this thread handles everything to do with the second MCP3424
-// it collects and queues the power packets (voltage/current)
-void mcp3424_thread2() {
-    MCP3424* dev = nullptr;
-    while (true) {
-        // try opening the MCP3424 every ten seconds until it succeeds
-        while (true) {
-            try {
-                dev = new MCP3424(0x6A, CHANNEL1 | ONESHOT | RES_16_BITS | PGAx2);
-                printf("Successfully opened the power MCP3424\n");
-                break;
-            } catch (int e) {
-                printf("FAILED to open power MCP3424. Trying again in 10 seconds...\n");
-                sleep(10);
-            }
-        }
-
-        PowerPacket* pPacket = nullptr;
-
-        try {
-            // get and queue the PowerPacket repeatedly
-            while (true) {
-                pPacket = new PowerPacket;
-                dev->setConfig(CHANNEL3 | ONESHOT | RES_16_BITS | PGAx1);
-                dev->startConversion();
-                while (!dev->isReady()) usleep(1000);
-                pPacket->voltageTime = getTimestamp();
-                pPacket->voltage = dev->getConversion();
-
-                dev->setConfig(CHANNEL4 | ONESHOT | RES_16_BITS | PGAx8);
-                dev->startConversion();
-                while (!dev->isReady()) usleep(1000);
-                pPacket->amperageTime = getTimestamp();
-                pPacket->amperage = dev->getConversion();
-
-                queue.push_tlm(pPacket);
-            }
-        } catch (int e) {
-            printf("Lost connection with power MCP3424\n");
-            delete dev;
-            continue;
-        }
-    }
-}
-
-// this thread watches the RPM interrupts through rpmISR(), then
-// constructs the RpmPacket and queues it
-void rpm_thread() {
-    RpmPacket* rPacket = nullptr;
-    // start the ISR (Interrupt Sub-Routine)
-    wiringPiSetup();
-    wiringPiISR(RPM_PIN, INT_EDGE_RISING, rpmISR);
-    unsigned long start = 0;
-    unsigned long end = count;
-    while (true) {
-        rPacket = new RpmPacket;
-        start = end;
-        //TODO make the delay more accurate
-        sleep(1);
-        end = count;
-        rPacket->value = 15 * (start - end - 1500);
-        rPacket->timestamp = getTimestamp();
-        queue.push_tlm(rPacket);
     }
 }
 
@@ -288,9 +218,7 @@ void housekeeping() {
 int main() {
     // launch each thread
     std::thread(ads1148_thread).detach();
-    std::thread(mcp3424_thread1).detach();
-    std::thread(mcp3424_thread2).detach();
-    std::thread(rpm_thread).detach();
+    std::thread(mcp3424_thread).detach();
     std::thread(housekeeping).detach();
 
     // This code changes the thread priority
@@ -300,18 +228,19 @@ int main() {
     if (pid > 0) system(cmd.str().c_str());*/
 
     // TODO: gracefully handle disconnects? Or not connected?
-    float speed;
+    // (max code) / ((max dac output/max controller input)*(voltage supply))
+    const float VOLTAGE_TO_DAC_CODE = 4095.0 / ((3.3/5.0)*(28.0));
+    int code;
     Packet* cmdPacket = nullptr;
-    DCMotor* pump = nullptr;
-    // try to connect to the motor hat every 10 seconds until it succeeds
+    MCP4725* dac = nullptr;
+    // try to connect to the DAC every 10 seconds until it succeeds
     while (true) {
         try {
-            // TODO: change PWM frequency?
-            pump = new DCMotor(1, 0x60, 1500);
-            printf("Successfully opened the motor hat\n");
+            dac = new MCP4725(0x60);
+            printf("Successfully opened the DAC\n");
             break;
         } catch (int e) {
-            printf("FAILED to open the motor hat. Trying again in 10 seconds...\n");
+            printf("FAILED to open the DAC. Trying again in 10 seconds...\n");
             sleep(10);
         }
     }
@@ -322,22 +251,17 @@ int main() {
             queue.pop_cmd(cmdPacket);
             PumpCmd* pCmd = static_cast<PumpCmd*>(cmdPacket);
             pCmd->PumpCmd::convert();
-            // convert the voltage into a PWM code
-            speed = pCmd->voltage * 4095.0 / 12.0;
-            printf("Received pump command: setting pump voltage to %f\n", pCmd->voltage);
-            // set the speed
-            pump->setGradSpeed((int) speed);
+            // convert the voltage into a DAC code
+            code = round(pCmd->voltage * VOLTAGE_TO_DAC_CODE);
+            printf("Received pump command: setting pump voltage to %f (%d)\n", pCmd->voltage, code);
+            // set the DAC
+            dac->write(code);
             delete cmdPacket;
             cmdPacket = nullptr;
         }
         usleep(500000);
     }
     return 0;
-}
-
-// increment the count every time the RPI detects a rising edge
-void rpmISR() {
-    count++;
 }
 
 // return the system time in microseconds since unix epoch
